a86fe0675bb0693f036e5f6d0b2727d8322c1d80
FirestormDragonFive <37475637+FirestormDragonFive@users.noreply.github.com>
2018-03-24 15:07:07 +0100

    Add files via upload

--- gra/main.js
+++ gra/main.js
@@ -1,33 +1,34 @@
-var mainState= {
+var mainState = {
 	
 	preload: function(){
-	   game.load.image('bird','bird.png');
-	   game.load.image('pipe','pipe.png');
+		
+	   game.load.image('bird' ,'bird.png');
+	   game.load.image('pipe' ,'pipe.png');
    },
 	
    create: function() {
 	 
         game.stage.backgroundColor= '#4DD0E1';
-        game.physics.startSystem(Phase.Physics.ARCADE);
+		
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+		
 		this.bird = game.add.sprite(100,245, 'bird');
+		
 		game.physics.arcade.enable(this.bird);
 		
 		this.bird.body.gravity.y = 1000;
+		
 		var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
 		
 	   this.pipes = game.add.group();
 	   
-   },
-   update: function() {
-	   
-	   if (this.bird.y <0)
-		      this.restartGame();    
-	   
+	   this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 	  
-	   },
-	   
-	    jump: function() {
+   },
+   
+   
+    jump: function() {
 			
 		   this.bird.body.velocity.y = -350;
 				   
@@ -36,7 +37,20 @@ var mainState= {
 	   restartGame: function() {
 		   game.state.start('main');
 	  
-}
+		},
+   
+   
+   update: function() {
+	   
+	   if (this.bird.y <0 || this.bird.y > 490){
+		   this.restartGame(); 
+	   }
+	   
+   
+	   
+	  
+	   },
+	   
   
   addOnePipe: function(x,y) {
 	  
@@ -54,15 +68,17 @@ var mainState= {
 		 
 		 var hole = Math.floor(Math.random()*5)+1;
 		 
-		 for(var i=0; i < 6; i++){
-		 
-			 this.addOnePipe(400, i*50);
+		 for(var i=0; i < 8; i++){
+		    if (i != hole && i != hole + 1) {
+			 this.addOnePipe(400, i * 60 + 20);
+			 
+			}
 			 
 		 } 
 		 
-	 }
+	 },
 	  
-  }
+  };
   
 var game = new Phaser.Game(400, 490);
 game.state.add('main', mainState);
